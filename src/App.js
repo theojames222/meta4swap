b9f53af49955b802de8e6b281a640145e27210b1
Your Name <you@example.com>
2022-05-02 18:22:32 -0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Create from "./components/pages/Create";
+import Shop from "./components/pages/Shop";
+import Product from "./components/pages/Product";
+import Order from "./components/pages/Order";
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>{/* Content */}</main>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/order" element={<Order />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,22 +5,22 @@ function Navbar() {
   return (
     <nav className="navbar mb-12 shadow-lg">
       <div className="container">
-        <div className="flex-none px-2 mx-2 ">
+        <Link to="/" className="flex-none px-2 mx-2 ">
           <img src={Meta4Swap} alt="" className="inline text-3xl" />
           {/* <Link to="/" className="text-lg font-bold align-middle">
       {title}
     </Link> */}
-        </div>
+        </Link>
         <nav className="navRight">
           <div className="flex ">
             <Link
-              to="/"
+              to="/shop"
               className="navShop btn btn-ghost btn-lg text-xl rounded-btn mr-12"
             >
               Shop
             </Link>
             <Link
-              to="/about"
+              to="/create"
               className="navShop btn btn-ghost btn-lg text-xl rounded-btn mr-12"
             >
               Create
