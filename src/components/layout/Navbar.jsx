5f51fe65ebb0a5fff3cff67ccc0270b5f4f709d6
Your Name <you@example.com>
2022-05-02 17:32:41 -0400

    initial commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import Meta4Swap from "../assets/Meta4Swap.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar mb-12 shadow-lg">
+      <div className="container">
+        <div className="flex-none px-2 mx-2 ">
+          <img src={Meta4Swap} alt="" className="inline text-3xl" />
+          {/* <Link to="/" className="text-lg font-bold align-middle">
+      {title}
+    </Link> */}
+        </div>
+        <nav className="navRight">
+          <div className="flex ">
+            <Link
+              to="/"
+              className="navShop btn btn-ghost btn-lg text-xl rounded-btn mr-12"
+            >
+              Shop
+            </Link>
+            <Link
+              to="/about"
+              className="navShop btn btn-ghost btn-lg text-xl rounded-btn mr-12"
+            >
+              Create
+            </Link>
+            <nav className="flex mt-1">
+              <button className="btnConnect text-white text-xl ">
+                Connect
+              </button>
+            </nav>
+          </div>
+        </nav>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
