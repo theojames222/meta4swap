f38b7ef9f024d03b9a80556fc86706cc1ee21cf8
Your Name <you@example.com>
2022-05-27 19:39:06 -0400

    transaction buttons updated

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../layout/Hero";
 import Content from "../layout/Content";
 import FeaturedProduct from "../layout/FeaturedProduct";
 import FeaturedService from "../layout/FeaturedService";
+
 function Home() {
   return (
     <>
